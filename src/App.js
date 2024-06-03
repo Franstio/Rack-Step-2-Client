26d785f4bfc60023ecd13530777c11154cf4ae90
FahriEfendi <fahriefendi0795@gmail.com>
2024-06-03 18:05:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx"
+import Timbangan from "./components/Timbangan.jsx"
 
 function App() {
   return (
     <div>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Timbangan />} />
+          <Route path="/bin" element={<Home />} />
         </Routes>
       
     </div>
