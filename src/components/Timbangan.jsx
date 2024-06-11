c92bf11d68f9b95d251368e064c712dbe554e931
FahriEfendi <fahriefendi0795@gmail.com>
2024-06-11 11:43:56 +0700

    .

--- src/components/Timbangan.jsx
+++ src/components/Timbangan.jsx
@@ -711,8 +711,8 @@ const Home = () => {
                                 onChange={e => setScanData(e.target.value)}
                                 value={scanData}
                                 name="text"
-                                ref={inputRef}
                                 onKeyDown={e => handleKeyPress(e)}
+                                ref={inputRef}
                                 className="block w-full rounded-md border-0 py-2 px-4 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="luGGIatKmKvdMkcxpKc8SZD64ex5W0"
                             />
